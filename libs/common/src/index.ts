d0b64935e7ebc1b4fa18a4ff83eed4075f0da875
SalischevAV <devorer77@gmail.com>
2024-01-09 18:10:24 +0100

    created mongo module

---
+++ libs/common/src/index.ts
@@ -0,0 +1,7 @@
+export * from './mongodbModule/mongodbModule.module';
+export * from './mongodbModule/abstract.repository';
+export * from './mongodbModule/abstract.schema';
+// export * from './rmq/rmq.service';
+// export * from './rmq/rmq.module';
+// export * from './auth/auth.module';
+// export * from './auth/jwt-auth.guard';
